fa14d304797aa8cf300d12916e5b682e8a0d0610
Krishna Kota <kota_k@apple.com>
2023-03-07 20:21:42 +0530

    Exercise2 & Exercise3 done

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -1,5 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Flux;
+
 import java.io.IOException;
 
 public class Exercise2 {
@@ -9,10 +11,12 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        final Flux<Integer> integerFlux = ReactiveSources.intNumbersFlux();
+        integerFlux.subscribe(System.out::println);
+        integerFlux.subscribe(e -> System.out.println("Another one " + e));
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
